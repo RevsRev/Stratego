303ab9467149dfe81024327ebc992bf1704db3af
Eddie Revell <eddie.revell@yahoo.co.uk>
2022-02-21 21:44:54 +0000

    Created a server command line.

--- Assets/Scripts/UnityAPI/StgBoardController.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StgBoardController : MonoBehaviour
-{
-   //currently there isn't anything to do for this
-}
