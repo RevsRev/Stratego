dea356b9ed8d9dafa5a0b0eab063ebf72ddda1fb
Eddie Revell <eddie.revell@yahoo.co.uk>
2022-03-22 20:23:56 +0000

    Started writing json messages for server-client communication

---
+++ Assets/Scripts/Bridge/StgJsonMsg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+
+public class StgJsonMsg
+{
+    public String msgType = "";
+    public StgAbstractJsonMsgContent content = null;
+
+    /*
+     * This method will only be used when deserialising messages.
+     */
+    public StgJsonMsg(String msgType, StgAbstractJsonMsgContent content)
+    {
+        this.msgType = msgType;
+        this.content = content;
+    }
+
+    /*
+     * The method we should always be using for creating messages.
+     */
+    public StgJsonMsg(StgAbstractJsonMsgContent content)
+    {
+        this.msgType = getMsgTypeForContent(content);
+        this.content = content;
+    }
+
+    private string getMsgTypeForContent(StgAbstractJsonMsgContent content)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string toJson()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+    public static StgJsonMsg fromJson(String json)
+    {
+        return JsonSerializer.Deserialize<StgJsonMsg>(json);
+    }
+}
---
+++ Assets/Scripts/Bridge/StgJsonMsgMovePiece.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class StgJsonMsgMovePiece : StgAbstractJsonMsgContent
+{
+    public int pieceId;
+    public Tuple<int, int> pos;
+
+    public StgJsonMsgMovePiece(int pieceId, Tuple<int,int> pos)
+    {
+        this.pieceId = pieceId;
+        this.pos = pos;
+    }
+}
